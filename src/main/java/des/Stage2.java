11454370a793ed8eca0786f212a460420c3d3a8e
anmrdz <anmrdz@gmail.com>
2016-09-10 21:34:27 -0500

    added expansion and permutation to F

--- src/main/java/des/Stage2.java
+++ src/main/java/des/Stage2.java
@@ -1,4 +1,5 @@
 
+package des;
 
 import java.util.BitSet;
 import java.util.List;
@@ -20,13 +21,39 @@ public class Stage2 {
 
     public BitSet innerF(BitSet ri, BitSet key){
 
-        //Do expansion
-        ri.xor(key);
+        //Expansion
+        int [] E = {32, 1, 2, 3, 4, 5,
+                    4, 5, 6, 7, 8, 9,
+                    8, 9, 10, 11, 12, 13,
+                    12, 13, 14, 15, 16, 17,
+                    16, 17, 18, 19, 20, 21,
+                    20, 21, 22, 23, 24, 25,
+                    24, 25, 26, 27, 28, 29,
+                    28, 29, 30, 31, 32, 1};
 
+        BitSet expandedRi = new BitSet(48);
+
+        for(int i = 0; i < E.length; i++){
+            expandedRi.set(i, ri.get(E[i]));
+        }
+
+        expandedRi.xor(key);
+
+        BitSet subRi = new BitSet(32);
         //S-boxes
-        //Permutation
 
-        return ri;
+        BitSet permRi = new BitSet(32);
+        //Permutation
+        int [] P = {16, 7, 20, 21, 29, 12, 28, 17,
+                    1, 15, 23, 26, 5, 18, 31, 10,
+                    2, 8, 24, 14, 32, 27, 3, 9,
+                    19, 13, 30, 6, 22, 11, 4, 25};
+
+        for (int i = 0; i < P.length; i++) {
+            permRi.set(i, subRi.get(P[i]));
+        }
+
+        return permRi;
     }
 
     public void rounds (BitSet m0, List<BitSet> keys) {
