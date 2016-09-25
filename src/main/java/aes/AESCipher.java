1b82034c4725d94f37f7d1023b3002fdd0fe86f6
anmrdz <anmrdz@gmail.com>
2016-09-24 21:28:08 -0500

    Added mixColumns and finished AES encryption and decryption

--- src/main/java/aes/AESCipher.java
+++ src/main/java/aes/AESCipher.java
@@ -1,6 +1,9 @@
 package aes;
 
 import java.util.Arrays;
+import java.util.List;
+
+import static aes.AuxTransformations.*;
 
 
 public class AESCipher {
@@ -17,27 +20,74 @@ public class AESCipher {
      * @param message - An array containing the numerical codes of the message. Its length must be 128.
      * @return bitSetsMessage - The bit sets with the correct order for using AES.
      */
-    public int[][] prepareMessage(int[] message) {
+    public static int[][] prepareMessage(int[] message) {
         int[][] matrix;
         int n = 4;
         if (!(message.length == 128 / 8)) {
             throw new RuntimeException("The message block length must be 128.");
         }
 
-        matrix = AuxTransformations.convertToMatrix(message, n, n);
-        matrix = AuxTransformations.transpose(matrix);
+        matrix = convertToMatrix(message, n, n);
+        matrix = transpose(matrix);
         return matrix;
     }
 
-    public static void main(String[] args) {
-        String message = "Aes Æs mÜÿ fáÇîl";
-        int[] textArray = message.chars().toArray();
+    public static int[] encrypt(int[] m, int[] key, Type type) {
 
-        AESCipher aesCipher = new AESCipher();
-        int[][] preparedMessage = aesCipher.prepareMessage(textArray);
+        int[][] message = prepareMessage(m);
+        List<int[][]> roundKeys = KeyGenerator.generate(key, type);
+        int[][] state = addRoundKey(message, roundKeys.get(0));
 
-        System.out.println(Arrays.deepToString(preparedMessage));
+        for (int i = 1; i < roundKeys.size(); i++) {
+            subBytes(state, false);
+            shiftRows(state, false);
 
+            if (i + 1 != roundKeys.size())
+                state = mixColumns(state, false);
+
+            addRoundKey(state, roundKeys.get(i));
+        }
+
+        return matrixToArray(transpose(state));
     }
 
+
+    public static int[] decrypt(int[] c, int[] key, Type type) {
+
+        int[][] ciphertext = prepareMessage(c);
+        List<int[][]> roundKeys = KeyGenerator.generate(key, type);
+        int[][] state = addRoundKey(ciphertext, roundKeys.get(roundKeys.size() - 1));
+
+        shiftRows(state, true);
+        subBytes(state, true);
+
+        for (int i = roundKeys.size() - 2; i >= 0; i--) {
+
+            if (i == 0) {
+                addRoundKey(state, roundKeys.get(i));
+                continue;
+            }
+
+            addRoundKey(state, roundKeys.get(i));
+            state = mixColumns(state, true);
+            shiftRows(state, true);
+            subBytes(state, true);
+        }
+
+        return matrixToArray(transpose(state));
+    }
+
+    public static void main(String[] args) {
+        String m = "Aes Æs mÜÿ fáÇîl";
+        //int[] message = {0x41, 0x45, 0x53, 0x20, 0x65, 0x73, 0x20, 0x6d, 0x75, 0x79, 0x20, 0x66, 0x61, 0x63, 0x69, 0x6c};
+        int[] key = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
+        int[] message = m.chars().toArray();
+
+        int[] ciphertext = encrypt(message, key, Type.AES128);
+        int[] plaintext = decrypt(ciphertext, key, Type.AES128);
+
+        System.out.println("message " + Arrays.deepToString(convertToMatrix(message, 4, 4)));
+        System.out.println("cipher " + Arrays.toString(ciphertext));
+        System.out.println("plaint " + Arrays.toString(plaintext));
+    }
 }
--- src/main/java/aes/KeyGenerator.java
+++ src/main/java/aes/KeyGenerator.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static aes.AESCipher.prepareMessage;
+import static aes.AuxTransformations.*;
+
 public class KeyGenerator {
 
     //Round constant
@@ -18,13 +21,13 @@ public class KeyGenerator {
         for (int i = 0; i < word.length; i++) {
             row = word[i] >> 4;
             col = word[i] & 15;
-            word[i] = AuxTransformations.getSBox()[row][col];
+            word[i] = getSBox()[row][col];
         }
 
         return word;
     }
 
-    public static List<int[][]> generate(int[][] initialKey, AESCipher.Type type) {
+    public static List<int[][]> generate(int[] initialKey, AESCipher.Type type) {
 
         int n = 4, rounds = 10;
 
@@ -44,13 +47,13 @@ public class KeyGenerator {
 
         for (int i = 0; i <= rounds; i++) {
 
-            roundKeys.add(new int[4][4]);
+            roundKeys.add(new int[n][4]);
             int[][] roundKey = roundKeys.get(i);
 
             if (i == 0) {
-                for (int j = 0; j < 4; j++) {
+                for (int j = 0; j < n; j++) {
                     for (int k = 0; k < 4; k++) {
-                        roundKey[j][k] = initialKey[j][k];
+                        roundKey[j][k] = initialKey[j * 4 + k];
                     }
                 }
                 continue;
@@ -58,10 +61,10 @@ public class KeyGenerator {
 
             int[][] prevKey = roundKeys.get(i - 1);
 
-            roundKey[0] = subWord(AuxTransformations.rotateWord(prevKey[n - 1].clone(), 1));
+            roundKey[0] = subWord(rotateWord(prevKey[n - 1].clone(), 1));
             roundKey[0][0] ^= rcon[i];
 
-            for (int j = 0; j < 4; j++) {
+            for (int j = 0; j < n; j++) {
                 for (int k = 0; k < 4; k++) {
                     roundKey[j][k] ^= prevKey[j][k];
 
@@ -73,7 +76,7 @@ public class KeyGenerator {
         }
 
         for (int i = 0; i < roundKeys.size(); i++) {
-            roundKeys.set(i, AuxTransformations.transpose(roundKeys.get(i)));
+            roundKeys.set(i, transpose(roundKeys.get(i)));
         }
 
         return roundKeys;
@@ -83,18 +86,14 @@ public class KeyGenerator {
 
         int[] message = {0x41, 0x45, 0x53, 0x20, 0x65, 0x73, 0x20, 0x6d, 0x75, 0x79, 0x20, 0x66, 0x61, 0x63, 0x69, 0x6c};
         int[] key = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
-
-        AESCipher aesCipher = new AESCipher();
-
-        int[][] keyMatrix = AuxTransformations.convertToMatrix(key, 4, 4);
-        int[][] messageMatrix = aesCipher.prepareMessage(message);
+        int[][] messageMatrix = prepareMessage(message);
 
         System.out.println("Message");
         System.out.println(Arrays.deepToString(messageMatrix));
         System.out.println("key");
-        System.out.println(Arrays.deepToString(keyMatrix));
+        System.out.println(Arrays.toString(key));
         System.out.println("rounds keys");
-        List<int[][]> roundKeys = generate(keyMatrix, AESCipher.Type.AES128);
+        List<int[][]> roundKeys = generate(key, AESCipher.Type.AES128);
 
         for (int[][] r : roundKeys) {
             System.out.println(Arrays.deepToString(r));
